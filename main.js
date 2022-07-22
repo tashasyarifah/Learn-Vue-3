5cf04670d7ae0471758d26258708b89cb5ac1eed
Tasha Syarifah <66051012+tashasyarifah@users.noreply.github.com>
2022-07-22 14:57:25 +0700

    Lesson 2- 11

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      cart: 0,
+      premium: true,
+    };
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
+    },
+  },
+});
